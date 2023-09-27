65400f6625a6bda359b6ebc1a9705bde05af71da
Mateus Ferreira <carinhaemateus@gmail.com>
2023-09-26 22:20:01 -0300

    content created

---
+++ src/app/components/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "Star Vs As forças do mal chega ao fim.",
+        "description": "No ep 37 da quarta temporada de Star Vs As forças do mal, para derrotar os soldados a Star destruiu a magia, mas para isso a Hekapoo abre o portal muito conscientemente de que ela vive da magia e estava se auto condenando a morte!",
+        "photoCover":"https://celebrity.fm/wp-content/uploads/2022/02/Is-there-gonna-be-a-season-5-of-Star-vs.-the-Forces-of-Evil.jpg"
+    },
+    {
+        "id":"2",
+        "title": "Jackie Lynn convidou o Marco para ir ao baile",
+        "description": "Jakie chamou o marco para o baile mas mudou de ideia, preferiu um encontro a sós",
+        "photoCover": "https://pm1.aminoapps.com/6533/558c9516ba6a1e3f38b1fc4aef28320a5f8b3299_00.jpg"
+    }
+]
